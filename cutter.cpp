cd8459a7c4ab491a92f06affebd7ce6889b59599
mew6210 <mew.62@wp.pl>
2024-11-23 13:21:15 +0100

    Add project files.

---
+++ cutter.cpp
@@ -0,0 +1,41 @@
+﻿#include "cutter.h"
+
+
+int main()
+{
+
+
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    const char* helloWorld = "Congrats! You created your first window!";
+    SetTargetFPS(60);              
+    Line b1=Line();
+    drawParameters lineParams = drawParameters();
+    lineParams.color = RED;
+    lineParams.posX = GetScreenWidth()/2;
+    lineParams.length = 500;
+    lineParams.posY = 200;
+    lineParams.height = 500;
+    
+    
+    while (!WindowShouldClose())    
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText(helloWorld, (GetScreenWidth()-MeasureText(helloWorld,20))/2, GetScreenHeight() / 2, 20, LIGHTGRAY);
+        b1.draw(lineParams);
+
+
+        EndDrawing();
+        
+    }
+
+    CloseWindow();        
+
+
+	
+	return 0;
+}
---
+++ cutter.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+
+#include <iostream>
+#include"raylib.h"
+
+
+class drawParameters {
+public:
+	int posX = 0;
+	int posY = 0;
+	int length = 0;
+	int height = 0;
+	Color color = Color(0, 0, 0);
+
+	drawParameters() = default;
+
+};
+
+
+
+
+
+class Widget {
+public:
+	int height = 0;
+	int width = 0;
+
+	virtual void draw(drawParameters param)=0;
+
+	Widget() :height(0),width(0){};
+
+};
+
+
+
+
+class Line : public virtual Widget{
+
+public:
+
+
+	void draw(drawParameters param) override{
+		DrawLine(param.posX,
+			param.posY,
+			param.posX+param.length,
+			param.posY+param.height, 
+			param.color);
+	}
+
+	Line() : Widget() {};
+
+
+};
+
+
+
